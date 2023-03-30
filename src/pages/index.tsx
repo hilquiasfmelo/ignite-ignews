29db7a84dd4dc003d004590fb72a66ea244a727d
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2023-03-30 15:37:52 -0300

    Add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home({ product }: HomeProps) {
 
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span>👏 Hey, welcome</span>
+          <span>Hey, welcome</span>
           <h1>News about <br /> the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
